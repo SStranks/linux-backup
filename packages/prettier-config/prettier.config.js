65aca56554fbebab502c7cca2d8e7918d6a507ad
Simon Stranks <sstranks87@gmail.com>
2025-07-12 15:51:26 +0100

    chore(config): added prettier,editorconfig,local megalint

---
+++ packages/prettier-config/prettier.config.js
@@ -0,0 +1,19 @@
+export default {
+  printWidth: 120,
+  tabWidth: 2,
+  semi: true,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  bracketSameLine: true,
+  arrowParens: "always",
+  trailingComma: "es5",
+  overrides: [
+    {
+      files: ["*.yml", "*.yaml"],
+      options: {
+        bracketSpacing: false,
+      },
+    },
+  ],
+};
